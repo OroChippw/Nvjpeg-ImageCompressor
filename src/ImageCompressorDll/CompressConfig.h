361d76b410945993258079876656ce3e5fbd6c87
OroChippw <OroChippw@163.com>
2023-05-25 13:35:44 +0800

    [WIP]feat:func Build ROI val

--- src/ImageCompressorDll/CompressConfig.h
+++ src/ImageCompressorDll/CompressConfig.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 struct CompressConfiguration {
     std::string input_dir;
@@ -16,4 +17,11 @@ struct CompressConfiguration {
     bool save_mat = false;
     bool save_binary = true;
     bool do_val = false;
+
+    bool use_roi = false;
+    int roi_w = 10;
+    int roi_h = 10;
+
+    cv::Rect roi_rect;
+    
 };
