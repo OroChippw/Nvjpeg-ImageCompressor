980d404f3955d7b1009be7d6c8429f01a432121c
OroChippw <OroChippw@163.com>
2023-06-16 10:49:49 +0800

    feat:func Add calculate cost time

--- src/ImageCompressorDll/ImageCompressor.cpp
+++ src/ImageCompressorDll/ImageCompressor.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 
 #include "ImageCompressor.h"
 #include "ImageCompressorImpl.h"
@@ -17,12 +18,20 @@ NvjpegCompressRunner::~NvjpegCompressRunner()
 
 void NvjpegCompressRunner::compress(CompressConfiguration cfg)
 {
+    auto startTime = std::chrono::steady_clock::now();
     std::string run_state = compressor->CompressImage(cfg) ? "Failure" : "Finish";
+    auto endTime = std::chrono::steady_clock::now();
     std::cout << "=> Compress " << run_state << std::endl;
+    auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+    std::cout << "Compress func cost time: " << elapsedTime << " ms" << std::endl;
 }
 
 void NvjpegCompressRunner::reconstruct(CompressConfiguration cfg , std::string ImageDirPath)
 {
+    auto startTime = std::chrono::steady_clock::now();
     std::string run_state = compressor->ReconstructedImage(cfg , ImageDirPath) ? "Failure" : "Finish";
+    auto endTime = std::chrono::steady_clock::now();
     std::cout << "=> Reconstructed " << run_state << std::endl;
+    auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+    std::cout << "Reconstruct func cost time: " << elapsedTime << " ms" << std::endl;
 }
